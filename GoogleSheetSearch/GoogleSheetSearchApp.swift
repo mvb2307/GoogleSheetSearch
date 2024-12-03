7a4ffff8fc7307a177327ebcce197bab00d5c3d4
Martijn van Beek <martijnvbpictures@gmail.com>
2024-12-03 21:18:00 +0100

    Initial Commit

---
+++ GoogleSheetSearch/GoogleSheetSearchApp.swift
@@ -0,0 +1,17 @@
+//
+//  GoogleSheetSearchApp.swift
+//  GoogleSheetSearch
+//
+//  Created by Martijn van Beek on 03/12/2024.
+//
+
+import SwiftUI
+
+@main
+struct GoogleSheetSearchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
